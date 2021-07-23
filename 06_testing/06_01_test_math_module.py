cebd7584b0af95c18b20e103832687cb8032e5e4
martin-martin <breuss.martin@gmail.com>
2021-07-23 20:57:20 +0200

    Remove leftover code

--- 06_testing/06_01_test_math_module.py
+++ 06_testing/06_01_test_math_module.py
@@ -1,17 +1,2 @@
 # Write a unittest test suite with at least two methods that test
 # the functionality of the built-in `math` module.
-
-import unittest
-import math
-
-
-class TestMath(unittest.TestCase):
-
-    def test_floor_rounds_down(self):
-        self.assertEqual(math.floor(3.4), 3)
-
-    def test_isnan_recognizes_numbers(self):
-        self.assertFalse(math.isnan(4))
-
-if __name__ == "__main__":
-    unittest.main()
