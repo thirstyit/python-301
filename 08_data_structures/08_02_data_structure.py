ff053b9e7936493d9436c940b0e739e95cc1d070
CodingNomads Dev <ryan@codingnomads.co>
2024-01-15 20:18:55 -0800

    Create 08_02_data_structure.py

---
+++ 08_data_structures/08_02_data_structure.py
@@ -0,0 +1,3 @@
+# Pick one of the abstract data structures mentioned in this section that you have not yet implemented
+# Build a custom Python class that demonstrates its functionality 
+# Compare your solution to: https://github.com/david-legend/python-algorithms/tree/main/data-structures/src
