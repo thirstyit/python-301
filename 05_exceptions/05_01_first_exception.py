b7b3fca0e8bc7610091bad35ec9b933a80ca2c39
Sam Ferguson <sam@thirstyit.com.au>
2024-07-07 13:39:39 +1000

    did exception ex 1 & 2

--- 05_exceptions/05_01_first_exception.py
+++ 05_exceptions/05_01_first_exception.py
@@ -2,7 +2,18 @@
 # Handle this exception with a try/except block.
 # For example:
 #
-# list_ = ["hello world!"]
-# print(list_[1])
+list_ = ["hello world!"]
+
+
+
+try:
+    print(list_[1])
+except:
+    print("Out of range, silly!")
 #
 # This raises and exception that needs to be handled.
+
+
+
+
+
--- 05_exceptions/05_02_calculator.py
+++ 05_exceptions/05_02_calculator.py
@@ -5,3 +5,18 @@
 # - if the user enters a zero as the divisor
 #
 # Test it and make sure it does not crash when you enter incorrect values.
+
+
+
+
+try:
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter a divisor: "))
+
+    result = num1 / float(num2)
+
+    print(result)
+except ValueError:
+    print("Integers only bro")
+except ZeroDivisionError:
+    print("Can't divide by zero derr.")
