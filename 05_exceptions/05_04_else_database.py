30e288361597c44cf7549e3e0da35c0e0b6a59df
Sam Ferguson <sam@thirstyit.com.au>
2024-07-07 14:14:03 +1000

    ex 4 done from 5

--- 05_exceptions/05_04_else_database.py
+++ 05_exceptions/05_04_else_database.py
@@ -2,3 +2,23 @@
 # For example, you can revisit the course module about database interactions
 # and include a try/except/else statement based on what to do whether or not
 # the database connection can be established.
+
+
+import sqlalchemy
+#from projects import keys 
+
+try:
+    engine = sqlalchemy.create_engine('mysql+pymysql://'+ 'wrong' + ':' + 'wrong' + '@localhost/recipes')
+    connection = engine.connect()
+
+    metadata = sqlalchemy.MetaData()  
+    recipe_table = sqlalchemy.Table('recipe', metadata, autoload_with=engine)
+
+except:
+    print("Connection Error")
+else:
+    stmt = sqlalchemy.select(recipe_table.columns.recipetitle)
+    results = connection.execute(stmt)
+    output = results.fetchall()
+
+    print(output)
