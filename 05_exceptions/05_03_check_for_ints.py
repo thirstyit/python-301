822ee20c4988e36c9ee5ccf231081eb890743f46
Sam Ferguson <sam@thirstyit.com.au>
2024-07-07 14:02:34 +1000

    ex 3 from 5

--- 05_exceptions/05_03_check_for_ints.py
+++ 05_exceptions/05_03_check_for_ints.py
@@ -2,3 +2,16 @@
 # validity of the input type, and displays a message depending on whether
 # the input was an integer or not.
 # The script should keep prompting the user until they enter an integer.
+
+v = True
+
+
+while(v) :
+    try:
+        x = int(input("Enter a number: ")) 
+    except ValueError:
+        print("Integers only bro.")
+    else:
+        v = False
+        
+        
